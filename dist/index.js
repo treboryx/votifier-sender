72935cf7c86724e5a1d826e8872b301870b59c04
Robert <treboryx@gmail.com>
2021-05-29 12:35:39 +0300

    cors

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const votifier_send_1 = __importDefault(require("votifier-send"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
 app.use(express_1.default.json());
-app.post('/', (req, res) => {
+app.use(cors_1.default());
+app.post("/", (req, res) => {
     const { settings } = req.body;
     settings.data.timestamp = new Date().getTime();
     try {
@@ -15,25 +17,27 @@ app.post('/', (req, res) => {
             if (err) {
                 return res.status(500).json({
                     success: false,
-                    message: 'Something went wrong - Server could be offline'
+                    message: "Something went wrong - Server could be offline",
                 });
             }
-            return res.status(500).json({ success: true, message: 'Votifier signal forwarded' });
+            return res
+                .status(500)
+                .json({ success: true, message: "Votifier signal forwarded" });
         });
     }
     catch (e) {
         return res.status(400).json({
             success: false,
             message: e,
-            friendly: 'Data provided (public key or IP) could be invalid'
+            friendly: "Data provided (public key or IP) could be invalid",
         });
     }
 });
-app.get('/', (req, res) => {
-    return res.redirect('https://crafty.gg');
+app.get("/", (req, res) => {
+    return res.redirect("https://crafty.gg");
 });
 app.listen(2000, () => {
-    console.log('Votifier-sender service started');
+    console.log("Votifier-sender service started");
 });
 module.exports = app;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import votifier from 'votifier-send';
-
+import express from "express";
+import votifier from "votifier-send";
+import cors from "cors";
 const app = express();
 
-app.use(express.json())
-
-app.post('/', (req, res) => {
+app.use(express.json());
+app.use(cors());
+app.post("/", (req, res) => {
   const { settings } = req.body as any;
   settings.data.timestamp = new Date().getTime();
   try {
@@ -13,26 +13,28 @@ app.post('/', (req, res) => {
       if (err) {
         return res.status(500).json({
           success: false,
-          message: 'Something went wrong - Server could be offline'
+          message: "Something went wrong - Server could be offline",
         });
       }
-      return res.status(500).json({ success: true, message: 'Votifier signal forwarded' });
+      return res
+        .status(500)
+        .json({ success: true, message: "Votifier signal forwarded" });
     });
   } catch (e) {
     return res.status(400).json({
       success: false,
       message: e,
-      friendly: 'Data provided (public key or IP) could be invalid'
+      friendly: "Data provided (public key or IP) could be invalid",
     });
   }
 });
 
-app.get('/', (req, res) => {
-  return res.redirect('https://crafty.gg');
+app.get("/", (req, res) => {
+  return res.redirect("https://crafty.gg");
 });
 
 app.listen(2000, () => {
-  console.log('Votifier-sender service started');
+  console.log("Votifier-sender service started");
 });
 
 module.exports = app;
